1962a5f27d8e9786d24831534cc8133a9ac8f7fd
j-koziel <jonathankoziel1@gmail.com>
2024-04-30 11:33:44 +0100

    fix: allow all origins

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ origins = [
 # CORS middleware to ensure that the frontend can make requests to the backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
