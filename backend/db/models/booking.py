4aa75b2e2dcd5177fae4a057546531aaa81871ee
j-koziel <jonathankoziel1@gmail.com>
2024-09-08 18:11:15 +0100

    feat: created sqlalchemy models for users, bookings and hotel rooms
    also added crud operations to update the database

--- backend/db/models/booking.py
+++ backend/db/models/booking.py
@@ -2,6 +2,21 @@ import uuid
 from typing import Literal
 
 from pydantic import BaseModel
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from db.db import Base
+
+
+class BookingModel(Base):
+    id: str = str(uuid.uuid4())
+    user = relationship("UserModel", back_populates="previous_bookings")
+    type: Literal["hotel", "zoo"]
+    guest: bool
+    date: str
+    num_people: int
+    email: str = ""
+    total_cost: str
 
 class Booking(BaseModel):
     id: str = str(uuid.uuid4())
--- backend/db/models/hotel_room.py
+++ backend/db/models/hotel_room.py
@@ -1,6 +1,23 @@
 from typing import Literal
 import uuid
+
 from pydantic import BaseModel
+from pydantic import BaseModel
+from sqlalchemy.orm import Mapped, mapped_column
+
+from db.db import Base
+
+class HotelRoomModel(Base):
+    id: Mapped[str] = mapped_column(primary_key=True)
+    room_number: Mapped[int]
+    dates_booked: Mapped[list[str]]
+    price_per_night: Mapped[str]
+    type: Mapped[Literal["Jungle Cabin", "Desert Cave", "Snowy Igloo"]]
+    description: Mapped[str]
+    num_beds: Mapped[int]
+    free_wifi: Mapped[bool] = mapped_column(False)
+    img_src: Mapped[str]
+    img_alt: Mapped[str]
 
 class HotelRoom(BaseModel):
     id: str = str(uuid.uuid4())
